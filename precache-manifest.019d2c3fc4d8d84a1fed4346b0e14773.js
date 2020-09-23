efa549ac414e6391450c4930d1cee4ca69ce43d3
Sourabh <hande.sourabh@gmail.com>
2020-09-23 18:33:22 +0530

    Updates

---
+++ precache-manifest.019d2c3fc4d8d84a1fed4346b0e14773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4d6ea4d7280b86784c7be870744b32",
+    "url": "/Anime4Life/index.html"
+  },
+  {
+    "revision": "3914978d76c16ebd0e9f",
+    "url": "/Anime4Life/static/css/main.b1d8cebe.chunk.css"
+  },
+  {
+    "revision": "2c4776423ea63642b3ff",
+    "url": "/Anime4Life/static/js/2.c1ac5e76.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Anime4Life/static/js/2.c1ac5e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3914978d76c16ebd0e9f",
+    "url": "/Anime4Life/static/js/main.aa8a8b42.chunk.js"
+  },
+  {
+    "revision": "a804e4b9471d6327112b",
+    "url": "/Anime4Life/static/js/runtime-main.c270b5ce.js"
+  },
+  {
+    "revision": "b649fef9872f689d3ea5b52c9a43cc31",
+    "url": "/Anime4Life/static/media/animeCharacters.b649fef9.jpg"
+  }
+]);
